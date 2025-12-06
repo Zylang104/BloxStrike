7c0e4b63f69ebd3ce255a6206f30859a0046f060
Zylang <joninurka7@gmail.com>
2025-12-06 12:19:15 +0100

    main script

---
+++ main.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Zylang104/BloxStrike/refs/heads/main/2dbox.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Zylang104/BloxStrike/refs/heads/main/aimbot.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Zylang104/BloxStrike/refs/heads/main/triggerbot.lua'))()
+print("all loaded perfectly")
